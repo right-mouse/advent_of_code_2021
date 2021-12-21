e89c418de07805a05ea3ba0139e48c90f0a08311
Arvind Suresh <arvind.suresh60@gmail.com>
2021-12-20 22:37:37 -0500

    Day 18

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod day_1;
+mod day_18;
 mod day_2;
 mod day_3;
 
@@ -24,6 +25,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         3 => match problem {
             1 => println!("{}", day_3::prob_1("input/day_3.txt")?),
             2 => println!("{}", day_3::prob_2("input/day_3.txt")?),
+            _ => return Err(format!("{} is an invalid problem", problem).into()),
+        },
+        18 => match problem {
+            1 => println!("{}", day_18::prob_1("input/day_18.txt")?),
+            2 => println!("{}", day_18::prob_2("input/day_18.txt")?),
             _ => return Err(format!("{} is an invalid problem", problem).into()),
         },
         _ => return Err(format!("{} is an invalid day", day).into()),
